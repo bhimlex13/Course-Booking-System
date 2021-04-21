08d9274b6c558e5cd2a0b4f48819eddc09657dc0
Glitch (hello-webpage) <none>
2021-04-21 12:05:06 +0000

    😂🏭 Checkpoint ./js/register.js:29751456/9953

--- js/register.js
+++ js/register.js
@@ -1,38 +1,52 @@
-const registerForm = document.querySelector('#register-form');
+const registerForm = document.querySelector("#register-form");
 
-registerForm.addEventListener('submit', (e) => {
+registerForm.addEventListener("submit", e => {
   e.preventDefault();
 
-  const firstName = e.target[0].value;
-  const lastName = e.target[1].value;
-  const mobileNumber = e.target[2].value;
-  const emailAddress = e.target[3].value;
-  const password = e.target[4].value;
-  const confirmPassword = e.target[5].value;
- 
+  const firstName = document.querySelector("#first-name").value;
+  const lastName = document.querySelector("#first-name").value;
+  const mobileNumber = document.querySelector("#first-name").value;
+  const firstName = document.querySelector("#first-name").value;
+  const firstName = document.querySelector("#first-name").value;
+  // const lastName = e.target[1].value;
+  // const mobileNumber = e.target[2].value;
+  // const emailAddress = e.target[3].value;
+  // const password = e.target[4].value;
+  // const confirmPassword = e.target[5].value;
 
-// Simple data validation here
-  const isUserDataValid = (firstName !== "" && lastName !== "" && mobileNumber.length === 13 && emailAddress !== "" && password !== "" && confirmPassword !== "" && password === confirmPassword);
+  // Simple data validation here
+  const isUserDataValid =
+    firstName !== "" &&
+    lastName !== "" &&
+    mobileNumber.length === 13 &&
+    emailAddress !== "" &&
+    password !== "" &&
+    confirmPassword !== "" &&
+    password === confirmPassword;
 
-  if(isUserDataValid) {
-   // Send a client request to the application sever
-    fetch(" https://alex-csp2-app-sever.herokuapp.com/api/users/register", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({
-           firstName,
-           lastName,
-           emailAddress,
-           password,
-           mobileNumber
-          })
+if(isUserDataValid) {
+    // Send a client request to the application server
+    fetch("https://alex-csp2-app-sever.herokuapp.com/api/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        emailAddress,
+        password,
+        mobileNumber
       })
-    .then()
-    .then();
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.dir(data);
+    });
     
   } else {
-  alert("Required field(s) is/are invalid.");
+    alert("Required field(s) is/are invalid.");
   }
 });
