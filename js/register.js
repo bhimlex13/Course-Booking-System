91dc68442b378512246c723c6d859b6cc44082db
Glitch (hello-webpage) <none>
2021-04-21 12:16:23 +0000

    🌝👫 Checkpoint ./js/register.js:29751456/17615

--- js/register.js
+++ js/register.js
@@ -1,35 +1,32 @@
-const registerForm = document.querySelector("#register-form");
+const registerForm = document.querySelector('#register-form');
 
-registerForm.addEventListener("submit", e => {
-  e.preventDefault();
+registerForm.addEventListener('submit', (e) => {
+  event.preventDefault();
 
-  const firstName = document.querySelector("#first-name").value;
-  const lastName = document.querySelector("#first-name").value;
-  const mobileNumber = document.querySelector("#first-name").value;
-  const firstName = document.querySelector("#first-name").value;
-  const firstName = document.querySelector("#first-name").value;
+  // const firstName = e.target[0].value;
   // const lastName = e.target[1].value;
   // const mobileNumber = e.target[2].value;
   // const emailAddress = e.target[3].value;
   // const password = e.target[4].value;
   // const confirmPassword = e.target[5].value;
+  
+  const firstName = document.querySelector("#first-name").value;
+  const lastName = document.querySelector("#last-name").value;
+  const mobileNumber = document.querySelector("#mobile-number").value;
+  const emailAddress = document.querySelector("#email-address").value;
+  const password = document.querySelector("#password").value;
+  const confirmPassword = document.querySelector("#confirm-password").value;
+  
 
-  // Simple data validation here
-  const isUserDataValid =
-    firstName !== "" &&
-    lastName !== "" &&
-    mobileNumber.length === 13 &&
-    emailAddress !== "" &&
-    password !== "" &&
-    confirmPassword !== "" &&
-    password === confirmPassword;
-
-if(isUserDataValid) {
+// Simple data validation here
+  const isUserDataValid = (firstName !== "" && lastName !== "" && mobileNumber.length === 13 && emailAddress !== "" && password !== "" && confirmPassword !== "" && password === confirmPassword);
+  
+    if (isUserDataValid) {
     // Send a client request to the application server
     fetch("https://alex-csp2-app-sever.herokuapp.com/api/users/register", {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-type": "application/json"
       },
       body: JSON.stringify({
         firstName,
@@ -39,14 +36,65 @@ if(isUserDataValid) {
         mobileNumber
       })
     })
-    .then(response => {
-      return response.json();
+      .then(response => {
+        return response.json();
     })
-    .then(data => {
-      console.dir(data);
+      .then(data => {
+         window.location.replace('/login.html');
     });
-    
   } else {
-    alert("Required field(s) is/are invalid.");
+    alert("Required field(s) is/are invalid");
   }
 });
+
+
+
+// const registerForm = document.querySelector("#register-form");
+
+// registerForm.addEventListener("submit", event => {
+//   event.preventDefault();
+//   // Registration process
+
+//   // Mini-activity
+//   // Get all the value of the fields needed to create a new user document
+//   const firstName = document.querySelector("#first-name").value;
+//   const lastName = document.querySelector("#last-name").value;
+//   const mobileNumber = document.querySelector("#mobile-number").value;
+//   const emailAddress = document.querySelector("#email-address").value;
+//   const password = document.querySelector("#password").value;
+//   const confirmPassword = document.querySelector("#confirm-password").value;
+
+//   // Simple data validation here
+//   const isUserDataValid =(
+//     firstName !== "" &&
+//     lastName !== "" &&
+//     mobileNumber.length === 11 &&
+//     emailAddress !== "" &&
+//     password !== "" &&
+//     confirmPassword !== "" &&
+//     password === confirmPassword);
+//   if (isUserDataValid) {
+//     // Send a client request to the application server
+//     fetch("https://henry-csp2-app-server.herokuapp.com/api/users/register", {
+//       method: "POST",
+//       headers: {
+//         "Content-type": "application/json"
+//       },
+//       body: JSON.stringify({
+//         firstName,
+//         lastName,
+//         emailAddress,
+//         password,
+//         mobileNumber
+//       })
+//     })
+//       .then(response => {
+//         return response.json();
+//     })
+//       .then(data => {
+//          window.location.replace('/login.html');
+//     });
+//   } else {
+//     alert("Required field(s) is/are invalid");
+//   }
+// });
