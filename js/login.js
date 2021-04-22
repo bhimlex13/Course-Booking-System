bd67ac626bd00a17c0ef64abb681f7d07c01e685
Glitch (hello-webpage) <none>
2021-04-22 09:54:41 +0000

    💘👆 Checkpoint ./js/login.js:29751456/942

--- js/login.js
+++ js/login.js
@@ -3,5 +3,15 @@ const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', (e) => {
   e.preventDefault();
   
-   // const emailAddress = e.target[3].value;
-})
+    const emailAddress = e.target[0].value;
+    const password = e.target[1].value;
+  
+    const isUserDataValid = emailAddress !== "" && password !== "";
+  
+  if (isUserDataValid){
+
+  } else {
+    alert("Missing Require Field(s)")
+  }
+    
+});
