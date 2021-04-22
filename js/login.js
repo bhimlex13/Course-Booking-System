740623b75af4f3c337566ece6246c727c552f0c5
Glitch (hello-webpage) <none>
2021-04-22 10:06:33 +0000

    🚙🏇 Checkpoint ./js/login.js:29751456/1624 ./js/register.js:29751456/1504

--- js/login.js
+++ js/login.js
@@ -1,17 +1,18 @@
-const loginForm = document.querySelector('#login-form');
+const loginForm = document.querySelector("#login-form");
 
-loginForm.addEventListener('submit', (e) => {
+loginForm.addEventListener("submit", e => {
   e.preventDefault();
-  
-    const emailAddress = e.target[0].value;
-    const password = e.target[1].value;
-  
-    const isUserDataValid = emailAddress !== "" && password !== "";
-  
-  if (isUserDataValid){
 
+  const emailAddress = e.target[0].value;
+  const password = e.target[1].value;
+
+  const isUserDataValid = emailAddress !== "" && password !== "";
+
+  if (isUserDataValid) {
+    fetch("", "")
+      .then()
+      .then();
   } else {
-    alert("Missing Require Field(s)")
+    alert("Missing Require Field(s)");
   }
-    
 });
--- js/register.js
+++ js/register.js
@@ -3,19 +3,19 @@ const registerForm = document.querySelector("#register-form");
 registerForm.addEventListener("submit", e => {
   event.preventDefault();
 
-  // const firstName = e.target[0].value;
-  // const lastName = e.target[1].value;
-  // const mobileNumber = e.target[2].value;
-  // const emailAddress = e.target[3].value;
-  // const password = e.target[4].value;
-  // const confirmPassword = e.target[5].value;
+  const firstName = e.target[0].value;
+  const lastName = e.target[1].value;
+  const mobileNumber = e.target[2].value;
+  const emailAddress = e.target[3].value;
+  const password = e.target[4].value;
+  const confirmPassword = e.target[5].value;
 
-  const firstName = document.querySelector("#first-name").value;
-  const lastName = document.querySelector("#last-name").value;
-  const mobileNumber = document.querySelector("#mobile-number").value;
-  const emailAddress = document.querySelector("#email-address").value;
-  const password = document.querySelector("#password").value;
-  const confirmPassword = document.querySelector("#confirm-password").value;
+  // const firstName = document.querySelector("#first-name").value;
+  // const lastName = document.querySelector("#last-name").value;
+  // const mobileNumber = document.querySelector("#mobile-number").value;
+  // const emailAddress = document.querySelector("#email-address").value;
+  // const password = document.querySelector("#password").value;
+  // const confirmPassword = document.querySelector("#confirm-password").value;
   
   let userD = {
       firstName,
@@ -35,6 +35,17 @@ registerForm.addEventListener("submit", e => {
 
   if (isUserDataValid) {
     // Send a client request to the application server
+    
+    // checkk email
+    fetch("https://alex-csp2-app-sever.herokuapp.com/api/users/check-email", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({ emailAddress  })
+    })
+    // Initiate the registration
+    
     fetch("https://alex-csp2-app-sever.herokuapp.com/api/users/register", {
       method: "POST",
       headers: {
