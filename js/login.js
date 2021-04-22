270474ef4ee7620a3541167598a7745816f62d0b
Glitch (hello-webpage) <none>
2021-04-22 09:43:37 +0000

    🎌🏞 Checkpoint ./login.html:29751456/19 ./js/login.js:29751456/219

---
+++ js/login.js
@@ -0,0 +1,7 @@
+const loginForm = document.querySelector('#login-form');
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+   // const emailAddress = e.target[3].value;
+})
