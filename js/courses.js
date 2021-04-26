3856b555ee159aee8111102927bee299b14f3481
Glitch (hello-webpage) <none>
2021-04-26 12:12:52 +0000

    💝🎌 Checkpoint ./css/style.css:29751456/30 ./courses.html:29751456/8 ./register.html:29751456/8 ./login.html:29751456/8 ./js/script.js:29751456/184 ./js/courses.js:29751456/180

--- js/courses.js
+++ js/courses.js
@@ -1,11 +1,3 @@
-let isAdmin = localStorage.getItem("isAdmin");
-
-// Mini-activity:
-// Create a variable to contain the val
-
-let userId = localStorage.getItem("userId");
-
-
 // Mini-activity:
 // Create a variable to contain the element with admi
 let createCourseContainer = document.querySelector("#admin-button");
@@ -14,6 +6,4 @@ let createCourseContainer = document.querySelector("#admin-button");
 if(!isAdmin || isAdmin == "false") {
   // is not admin
   createCourseContainer.innerHTML = null;
-} else {
-  // is an admin
 }
---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Check if user is logged-in
+
+let isAdmin = localStorage.getItem("isAdmin");
+
+// Mini-activity:
+// Create a variable to contain the val
+
+let userId = localStorage.getItem("userId");
