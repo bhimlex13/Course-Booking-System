594c79c16ccbc49e3dec936ae9a19be449549568
Glitch (hello-webpage) <none>
2021-04-26 10:39:44 +0000

    🚠🏨 Checkpoint ./js/courses.js:29751456/253 ./courses.html:29751456/9

---
+++ js/courses.js
@@ -0,0 +1,9 @@
+let isAdmin = localStorage.getItem("isAdmin");
+console.log(isAdmin);
+
+
+// Mini-activity:
+// Create a variable to contain the val
+
+let userId = localStorage.getItem("userId");
+console.log(userId);
