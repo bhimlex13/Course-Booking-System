75e41509456eea4a42dfc8fbdc551a9346727e44
Glitch (hello-webpage) <none>
2021-04-28 12:39:46 +0000

    🍯🍅 Checkpoint ./js/courses.js:29751456/1632

--- js/courses.js
+++ js/courses.js
@@ -1,4 +1,3 @@
-const coursesContainerElement = document.querySelector("#courses-container");
 // Mini-activity:
 // Create a variable to contain the element with admi
 let createCourseContainer = document.querySelector("#admin-button");
@@ -16,10 +15,37 @@ if (!isAdmin || isAdmin == "false") {
 fetch("https://alex-csp2-app-sever.herokuapp.com/api/courses")
 .then(res => res.json())
 .then(data => {
-  console.dir(data.courses);
-  for (var i = 1; i < 10; i++){
-    coursesContainerElement.innerHTML += data.courses[i];
-
-}
+  const coursesElement = data.courses.map(course => {
+  return `<div class="col-md-6 my-3">
+    <div class='card'>
+        <div class='card-body'>
+            <h5 class='card-title'>${course.name}</h5>
+            <p class='card-text text-left'>
+                ${course.description}
+            </p>
+            <p class='card-text text-right'>
+               ₱ ${course.price}
+            </p>
+        </div>
+        <div class='card-footer'>
+            <div class="row">
+              <div class="col-sm-12 col-md-4 mb-1">
+                <a href="./course.html?id=${course._id}" class="btn btn-outline-primary btn-block btn-course">View</a>
+              </div>
+              <div class="col-sm-12 col-md-4 mb-1">
+                <a href="./edit_course.html?id=${course._id}" class="btn btn-outline-secondary btn-block btn-course">Edit</a>
+              </div>
+              <div class="col-sm-12 col-md-4 mb-1">
+                <a href="./delete_course.html?id=${course._id}" class="btn btn-outline-danger btn-block btn-course">Delete</a>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>`
+  });
+  
+console.log(coursesElement.join(""));
+  
+  
 });
 
