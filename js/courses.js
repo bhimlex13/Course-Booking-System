9380d181611c58403af8e88fed210adb7eee11c3
Glitch (hello-webpage) <none>
2021-04-28 13:03:36 +0000

    💧🌨 Checkpoint ./js/courses.js:29751456/5423

--- js/courses.js
+++ js/courses.js
@@ -16,47 +16,51 @@ fetch("https://alex-csp2-app-sever.herokuapp.com/api/courses")
 .then(res => res.json())
 .then(data => {
   
-  if(isAdmin === null || isAdmin === "false") {
-    data.courses.filter(courses => {
-          
-    })
+   // Filter the active courses if user is unauthenticated or is non-admin
+  let coursesList;
+  if (isAdmin === null || isAdmin === "false") {
+    coursesList = data.courses.filter(course => {
+      if (course.isActive === true) {
+        return course;
+      }
+    });
   } else {
-
-
+    coursesList = data.courses;
   }
   
-  
-  const coursesElement = data.courses.map(course => {
-  return `<div class="col-md-6 my-3">
-    <div class='card'>
-        <div class='card-body'>
-            <h5 class='card-title'>${course.name}</h5>
-            <p class='card-text text-left'>
-                ${course.description}
-            </p>
-            <p class='card-text text-right'>
-               ₱ ${course.price}
-            </p>
-        </div>
-        <div class='card-footer'>
-            <div class="row">
-              <div class="col-sm-12 col-md-4 mb-1">
-                <a href="./course.html?id=${course._id}" class="btn btn-outline-primary btn-block btn-course">View</a>
+  // Display all courses on the course page
+  const coursesElements = coursesList.map(course => {
+    return `
+      <div class="col-md-6 my-3">
+          <div class='card'>
+              <div class='card-body'>
+                  <h5 class='card-title'>${course.name}</h5>
+                  <p class='card-text text-left'>
+                      ${course.description}
+                  </p>
+                  <p class='card-text text-right'>
+                     ₱ ${course.price}
+                  </p>
               </div>
-              <div class="col-sm-12 col-md-4 mb-1">
-                <a href="./edit_course.html?id=${course._id}" class="btn btn-outline-secondary btn-block btn-course">Edit</a>
+              <div class='card-footer'>
+                  <div class="row">
+                    <div class="col-sm-12 col-md-4 mb-1">
+                      <a href="./course.html?id=${course._id}" class="btn btn-outline-primary btn-block btn-course">View</a>
+                    </div>
+                    <div class="col-sm-12 col-md-4 mb-1">
+                      <a href="./edit_course.html?id=${course._id}" class="btn btn-outline-secondary btn-block btn-course">Edit</a>
+                    </div>
+                    <div class="col-sm-12 col-md-4 mb-1">
+                      <a href="./delete_course.html?id=${course._id}" class="btn btn-outline-danger btn-block btn-course">Delete</a>
+                    </div>
+                  </div>
               </div>
-              <div class="col-sm-12 col-md-4 mb-1">
-                <a href="./delete_course.html?id=${course._id}" class="btn btn-outline-danger btn-block btn-course">Delete</a>
-              </div>
-            </div>
-        </div>
-    </div>
-</div>`
+          </div>
+      </div>
+    `;
   });
   
-// console.log(coursesElement.join(""));
-  document.querySelector("#courses-container").innerHTML = coursesElement.join("");
+  // console.log(coursesElements.join(""));
   
+  document.querySelector("#courses-container").innerHTML = coursesElements.join("");
 });
-
