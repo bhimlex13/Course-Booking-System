50b49fcf4330f049dff15445d548da18fa3adbc8
Glitch (hello-webpage) <none>
2021-04-29 08:09:59 +0000

    🛳🛥 Checkpoint ./css/style.css:29751456/520 ./js/main.js:29751456/1037 ./index.html:29751456/69

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 
-
-
 /*===== MENU SHOW Y HIDDEN =====*/
 const navMenu = document.getElementById('nav-menu'),
 toggleMenu = document.getElementById('nav-toggle'),
@@ -66,17 +64,3 @@ var typed = new Typed(".typing", {
 
 
 
-// Sticky Nav Bar
-$(document).ready(function(){
-	$(window).scroll(function(){
-		if(this.scrollY > 20){
-			$('.navbar').addClass("sticky");
-		}else{
-			$('.navbar').removeClass("sticky");
-		}
-		if(this.scrollY > 500){
-			$('.scroll-up-btn').addClass("show");
-		}else{
-			$('.scroll-up-btn').removeClass("show");
-		}
-	});
