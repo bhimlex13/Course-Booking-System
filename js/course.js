d85b3251174ccc3fa6014eef23444a4283174b26
Glitch (hello-webpage) <none>
2021-04-29 12:28:29 +0000

    🐓🌕 Checkpoint ./js/course.js:29751456/361 ./course.html:29751456/578 ./css/style.css:29751456/97

--- js/course.js
+++ js/course.js
@@ -11,5 +11,11 @@ const courseId = queryStringObject.get("id");
 fetch("https://alex-csp2-app-sever.herokuapp.com/api/courses?id="+courseId)
 .then(res => res.json())
 .then(data => {
-      console.dir(data);
+      
+    const {name, description, price } =  data.course;
+    console.dir(data);
+  
+    document.querySelector("#course-name").innerText = name;
+    document.querySelector("#course-description").innerText = description;
+    document.querySelector("#course-price").innerText = price;
 });
