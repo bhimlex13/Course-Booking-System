c14f831a61d080fc812bf1052a71cab885fbec8e
Glitch (hello-webpage) <none>
2021-05-03 12:44:44 +0000

    👹🌖 Checkpoint ./js/course.js:29751456/14251

--- js/course.js
+++ js/course.js
@@ -4,21 +4,30 @@ const courseId = queryStringObject.get("id");
 
 // console.log(courseId)
 
-
-
 //
 
-fetch("https://alex-csp2-app-sever.herokuapp.com/api/courses?id="+courseId)
-.then(res => res.json())
-.then(data => {
-      
-    const {name, description, price } =  data.course;
+fetch("https://alex-csp2-app-sever.herokuapp.com/api/courses?id=" + courseId)
+  .then(res => res.json())
+  .then(data => {
+    const { name, description, price, enrollees } = data.course;
     // console.dir(data);
-  
+
     document.querySelector("#course-name").innerText = name;
     document.querySelector("#course-description").innerText = description;
     document.querySelector("#course-price").innerText = price;
-});
+    // console.dir(enrollees);
+
+    //Display all enrollees if user is an admin
+    const enrolleesList = enrollees.map(enrollee => {
+      const { firstName, lastName, emailAddress } = enrollees;
+      return ` 
+            <tr>
+                  <td>${lastName},${firstName}</td>
+                  <td>${emailAddress}</td>
+</tr>
+`;
+    });
+  });
 
 // Do not allow non-authenticated users or admins to enroll
 const btnEnroll = document.querySelector("#btn-enroll");
@@ -27,20 +36,20 @@ if (userId === null || isAdmin === "true") {
 } else {
   btnEnroll.addEventListener("click", event => {
     // console.log('enroll button was clicked!');
-    
-    fetch("https://alex-csp2-app-sever.herokuapp.com/api/users/enroll",{
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            userId,
-            courseId
-        })
+
+    fetch("https://alex-csp2-app-sever.herokuapp.com/api/users/enroll", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        userId,
+        courseId
+      })
     })
       .then(res => res.json())
       .then(data => {
-       // console.dir(data.message);  
+        // console.dir(data.message);
 
         if (data.message) {
           alert("New enrollment to a course was successful.");
@@ -48,6 +57,6 @@ if (userId === null || isAdmin === "true") {
         } else {
           alert("Error: New enrollment to a course encountered a problem.");
         }
-    });
+      });
   });
 }
